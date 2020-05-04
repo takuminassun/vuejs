4903d1f9bfd8fc79dc04df14ef916ac9f01ac531
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2020-05-04 15:41:46 +0900

    section12

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 var app = new Vue({
   el: '#app',
   data: {
-    show: true
-  },
-  methods: {
-    handleClick() {
-      var count = this.$refs.count
-      if(count) {
-        count.innerText = parseInt(count.innerText,10) + 1
-      }
-    }
+    message: "Hello <strong>Vue.js!</strong>"
   }
 })
