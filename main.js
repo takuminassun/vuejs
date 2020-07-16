03203ae372b4114971d6f0102d682922efdeed3a
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2020-07-17 07:16:14 +0900

    reduceメソッドを利用

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: "Hello <strong>Vue.js!</strong>"
+    items: [{ id: 1, name: 'りんご', price: 100 },
+           { id: 2, name: 'パイナップル', price: 140 },
+           { id: 3, name: 'マンゴー', price: 350 }]
+  },
+
+  computed: {
+    total() {
+      // 初めに書いていたコード
+      prices = this.items.map((obj) => obj.price)
+      let sum = 0
+      prices.forEach(element => {
+        sum += element
+      });
+      return sum
+
+      // reduceメソッドを教えてもらい書き直したコード
+      // return this.items.reduce((sum, obj) => sum + obj.price, 0)
+    }
   }
 })
